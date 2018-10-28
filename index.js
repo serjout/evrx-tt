db7ba17b4f2abbaca32864a5142c6b816290adfc
sergeyutkin <sergey.utkin@splyt.com>
2018-10-28 10:37:52 +0300

    OfferStore

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const{ theWeb3 } = require('./src/the-web3');
 const { theContract } = require('./src/the-contract');
 const BigNumber = require('bignumber.js');
-const { Offer } = require('./src/Offer');
+const { OfferStore } = require('./src/OfferStore');
 const { Token } = require('./src/Token');
 
 
@@ -11,7 +11,9 @@ const { Token } = require('./src/Token');
     const t1 = new Token('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2');
     const t2 = new Token('0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359');
 
-    const [res] = await Offer.getVolumePrice(t1, t2, BigInt('99923957565163766590012'));
+    const store = new OfferStore();
+    const {offers, ...result} = await store.getRecommendationsForVolumeBuy(t1, t2, BigInt('99923957565163766590012'));
 
-    console.log('>>>> ', res);
+    console.log('>>>> ', result);
+    console.log('>>>> ', offers.join('\n'));
 })();
--- src/Offer.js
+++ src/Offer.js
@@ -3,138 +3,10 @@ const { theWeb3, fromBlockPromise } = require('./the-web3');
 const { Token } = require('./Token');
 const { moveDecimalPoint } = require('./utils/move-decimal-point');
 
-const createDownCount = count => () => --count !== 0;
 const PRECISION = 20;
 const PRECISION_MUL = BigInt(10) ** BigInt(PRECISION);
 
 class Offer {
-    /**
-     * 
-     * @param {Token} token1 
-     * @param {Token} token2 
-     * @param {Offer => boolean} stopIfFalse 
-     */
-    static async getOffersUntil(token1, token2, stopIfFalse = createDownCount(20)) {
-        // let stopLogQueueBlocking;
-        // Offer._untilLogQueueBlock = new Promise(done => stopLogQueueBlocking = done);
-
-        const result = [];
-        const pairKey = this._getPairKey(token1, token2);
-        let curr;
-
-        if (Offer._mapPairToBest[pairKey] === undefined) {
-            const id = await theContract.methods.getBestOffer(token1.address, token2.address).call();
-            curr = await Offer.getById(id);
-            Offer._mapPairToBest[pairKey] = curr;
-            result.push(curr);
-        }
-
-        while(stopIfFalse(curr) && curr._next !== undefined) {
-            const next = await Offer.getById(curr._next);
-            next._prev = curr;
-            curr = next;
-            result.push(curr);
-        }
-
-        //stopLogQueueBlocking();
-        return result;
-    }
-
-    static async getBuyVolume(token1, token2, volume = BigInt(0)) {
-        let sumPay = BigInt(0);
-        let sumBuy = BigInt(0);
-
-        function predicate(offer) {
-            let stop = true;
-
-            const sumNext = sumBuy + offer.buy.amount;
-
-            if (volume <= sumNext) {
-                stop = false;
-
-                if (volume < sumNext) {
-                    const surplusBuy = sumNext - volume;
-                    const rate = surplusBuy * PRECISION_MUL / offer.buy.amount;
-                    const surplusPay = offer.pay.amount * rate / PRECISION_MUL;
-
-                    console.log('surplusPay', surplusPay);
-                    console.log('surplusPay sub', offer.pay.amount - surplusPay);
-                    console.log(moveDecimalPoint(offer.pay.amount * PRECISION_MUL / surplusPay, -PRECISION));
-                    console.log(moveDecimalPoint(offer.buy.amount * PRECISION_MUL / surplusBuy, -PRECISION));
-
-                    sumPay += offer.pay.amount - surplusPay;
-                    sumBuy += offer.buy.amount - surplusBuy;
-                }
-            } else {
-                sumBuy = sumNext;
-                sumPay += offer.pay.amount;
-            }
-
-            return stop;
-        }
-
-        const offers = await Offer.getOffersUntil(token1, token2, predicate);
-        console.log('sumBuy ', sumBuy, 'volume ', volume, 'sumPay', sumPay);
-
-        return [moveDecimalPoint(sumBuy * PRECISION_MUL / sumPay, -PRECISION), sumBuy, sumPay,  offers];
-    }
-
-    static async getPastOffers(token1, token2, limit = 10) {
-        const predicate = createDownCount(limit);
-
-        return Offer.getOffersUntil(token1, token2, predicate);
-    }
-
-    // static _addToLogQueue(events) {
-    //     Offer._logQueue.push.call(Offer._logQueue, events);
-
-    //     if (Offer._logQueuePromise === undefined) {
-    //         Offer._logQueuePromise = Offer._processLogQueue();
-    //     }
-    // }
-
-    // static async _processLogQueue() {
-    //     while(Offer._logQueue.length > 0) {
-    //         await this._untilLogQueueBlock();
-
-    //         const event = Offer._logQueue.shift();
-
-    //         // TODO
-    //         console.log(event)
-    //     }
-    //     Offer._logQueuePromise = undefined;
-    // }
-
-    static async syncOffers() {
-        let loop = true;
-        const fromBlock = await fromBlockPromise;
-        const cancel = () => loop = true;
-
-        
-        
-        return cancel;
-    }
-
-    static async getById(id) {
-        if (Offer._offers[id] === undefined) {
-            const [o, nextId] = await Promise.all([
-                theContract.methods.offers(id).call(),
-                theContract.methods.getWorseOffer(id).call(),
-            ]);
-            const offer = new Offer(id, o.owner, o.pay_gem, o.pay_amt, o.buy_gem, o.buy_amt, o.timestamp);
- 
-            offer._next = nextId;
-            Offer._offers[offer.id] = offer;
-        }
-
-        console.log('' + Offer._offers[id]);
-        return Offer._offers[id];
-    }
-
-    static _getPairKey(buyToken, payToken) {
-        return `${buyToken.ticker} / ${payToken.ticker}` 
-    }
-
     constructor(id, maker, payToken, payAmount, buyToken, buyAmount, timestamp) {
         if (arguments.length < 7 || Array.from(arguments).some(x => !x)) {
             throw new Error('Wrong argument');
@@ -179,13 +51,5 @@ class Offer {
         return `### Offer ${this.buy.token.ticker}/${this.pay.token.ticker} buy:${this.buy.amount} pay:${this.pay.amount} price:${this.decimalPrice}`;
     }
 }
-
-Offer._offers = {};
-Offer._eventLogTrade = null;
-Offer._offerLog = undefined;
-Offer._mapPairToBest = {};
-// Offer._logQueue = [];
-// Offer._untilLogQueueBlock = Promise.resolve();
-// Offer._logQueuePromise = undefined;
 
 module.exports = { Offer };
